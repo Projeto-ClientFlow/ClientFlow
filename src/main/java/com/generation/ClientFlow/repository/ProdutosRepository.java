fed82ea48bac1ff464e657bef2cf03b60be31845
Willa Evangelista <willaevangelista.pro@gmail.com>
2025-03-13 12:11:08 -0300

    Desenvolvimento da model e repository dos Produtos

--- src/main/java/com/generation/ClientFlow/repository/ProdutosRepository.java
+++ src/main/java/com/generation/ClientFlow/repository/ProdutosRepository.java
@@ -1,5 +1,14 @@
 package com.generation.ClientFlow.repository;
 
-public class ProdutosRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.ClientFlow.model.Produtos;
+
+public interface ProdutosRepository extends JpaRepository<Produtos, Long>{
+
+	public List<Produtos> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	
 }
